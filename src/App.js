a047552a601ff24cb819a01f318c52b6f6114ce9
Kike <kike@Kikes-MacBook-Pro.local>
2020-08-01 10:39:33 +0200

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//IMPORTS
+//-Modules
+import React from 'react'
+import styled, { createGlobalStyle } from 'styled-components'
 
+
+//STYLE
+const GlobalStyle = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background: #F1EFE7;
+  }
+`
+const BackgroundColor = styled.div`
+  background: #F1EFE7;
+  width: 100vw;
+  height: 100vh;
+`
+
+
+//MAIN COMPONENT
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <GlobalStyle/>
+      <BackgroundColor />
+    </>
+  )
 }
-
-export default App;
+export default App
